c5d92193d737cbb7ce05d45ec19716789a77b1a8
Anurag Singh <72293742+Anurag-Singh-Rajput@users.noreply.github.com>
2021-06-27 21:57:14 +0530

    form-added,responsive

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 anime({
-    targets : '.navbar-brand',
-    rotateZ : 360,
-    duration : 5000,
-    loop : true
+  targets: '.photo',
+  scale: [
+    {value: .1, easing: 'easeOutSine', duration: 900},
+    {value: 1, easing: 'easeInOutQuad', duration: 1200}
+  ],
+  delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
 });
 
+
 anime({
-    targets: '.photo',
-    scale: [
-      {value: .1, easing: 'easeOutSine', duration: 900},
-      {value: 1, easing: 'easeInOutQuad', duration: 1200}
-    ],
-    delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
-  });
+  targets: '.download-button',
+  scaleY: [
+    { value: [1, 1], duration: 1200 },
+    { value: 2, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+    { value: 1, duration: 900 },
+    { value: 1.75, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+    { value: 1, duration: 900 }
+  ],
+  easing: 'easeOutElastic(1, .8)',
+  loop: true
+});
